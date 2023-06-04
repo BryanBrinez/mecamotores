3198595fe87acab937889e53e202ad7a660effa1
BryanBrinez <94941570+BryanBrinez@users.noreply.github.com>
2023-06-04 16:19:38 -0500

    barra de buscar hecha

--- components/TablaAux.jsx
+++ components/TablaAux.jsx
@@ -1,43 +1,50 @@
 
+import React, { useState } from "react";
 
-export default function TablaAux({datos}) {
+export default function TablaAux({ datos }) {
+  const [selectedItem, setSelectedItem] = useState("");
+
+  console.log(selectedItem)
   return (
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Cliente
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            Cliente
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Modelo
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Tipo
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Marca
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Estado
+          </th>
+        </tr>
+      </thead>
+      {datos.map((cliente) => (
+        <tbody key={cliente._id}>
+          <tr
+            className="hover:bg-gray-50 dark:hover:bg-gray-600 bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+            onClick={() => setSelectedItem(cliente._id)}
+          >
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {cliente.Cliente.name}
             </th>
-            <th scope="col" className="px-6 py-3">
-              Modelo
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Tipo
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Marca
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Estado
-            </th>
+            <td className="px-6 py-4">{cliente.Maquina.modelo}</td>
+            <td className="px-6 py-4">{cliente.Maquina.tipo}</td>
+            <td className="px-6 py-4">{cliente.Maquina.marca}</td>
+            <td className="px-6 py-4">{cliente.estado}</td>
           </tr>
-        </thead>
-        {datos.map((cliente) => (
-          <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                {cliente.Cliente.name}
-              </th>
-              <td className="px-6 py-4">{cliente.Maquina.modelo}</td>
-              <td className="px-6 py-4">{cliente.Maquina.tipo}</td>
-              <td className="px-6 py-4">{cliente.Maquina.marca}</td>
-              <td className="px-6 py-4">{cliente.estado}</td>
-            </tr>
-          </tbody>
-        ))}
-      </table>
-  )
+        </tbody>
+      ))}
+    </table>
+  );
 }
