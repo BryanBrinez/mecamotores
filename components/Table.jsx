dd8cd55b64223c4b7c07bcf448c9122016d14fe2
Camilo110 <luissepulveda110@gmail.com>
2023-06-04 16:56:05 -0500

    Revert "b"
    This reverts commit dcea49c118e7815abe2a3be29f2c00ddc5b136b3, reversing
    changes made to 277ba49e9d81afaf7f26ee4ee3fb67e3889fbcc4.

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,71 +1,57 @@
 "use client";
 import { useState, useEffect } from "react";
-import TablaAux from "./TablaAux";
 
 export default function Table() {
   const [allMaqClient, setallMaqClient] = useState([]);
-  const [tablaMaqClient, setalltablaMaqClient] = useState([]);
-  const [busqueda, setBusqueda] = useState("");
 
   const fetchMaqClient = async () => {
     const response = await fetch("/api/ingresoMaquina");
     const data = await response.json();
     setallMaqClient(data);
-    setalltablaMaqClient(data);
   };
-
-  const handleChange = (e) => {
-    setBusqueda(e.target.value);
-    filtrar(e.target.value);
-  };
-
-  const filtrar = (terminoBusqueda) => {
-    var resultadosBusqueda = tablaMaqClient.filter((elemento) => {
-      if (
-        elemento.Cliente.name
-          .toString()
-          .toLowerCase()
-          .includes(terminoBusqueda.toLowerCase()) ||
-        elemento.Maquina.tipo
-          .toString()
-          .toLowerCase()
-          .includes(terminoBusqueda.toLowerCase()) ||
-        elemento.Maquina.marca
-          .toString()
-          .toLowerCase()
-          .includes(terminoBusqueda.toLowerCase())
-      ) {
-        return elemento;
-      }
-    });
-    setallMaqClient(resultadosBusqueda);
-  };
-
   useEffect(() => {
     fetchMaqClient();
   }, []);
 
   return (
     <div className="relative overflow-x-auto">
-      <div className="flex flex-col justify-center items-center gap-3">
-        <div className="bg-third-color items-center rounded-md flex justify-center mt-3 mb-2 gap-2 p-2">
-          <label htmlFor="inputField" className=" flex flex-row flex-shrink-0 ">
-            Seleccionar Maquina:
-          </label>
-          <input
-            type="text"
-            id="inputField"
-            value={busqueda}
-            onChange={handleChange}
-            className="block text-center w-full rounded-md border-0 py-1 px-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="codigo de la maquina"
-          />
-          <button className="bg-indigo-600 text-white rounded-md  px-4 py-1">
-            Buscar
-          </button>
-        </div>
-      </div>
-      <TablaAux datos={allMaqClient} />
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Cliente
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Modelo
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Tipo
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Marca
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Estado
+            </th>
+          </tr>
+        </thead>
+        {allMaqClient.map((cliente) => (
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {cliente.Cliente.name}
+              </th>
+              <td className="px-6 py-4">{cliente.Maquina.modelo}</td>
+              <td className="px-6 py-4">{cliente.Maquina.tipo}</td>
+              <td className="px-6 py-4">{cliente.Maquina.marca}</td>
+              <td className="px-6 py-4">{cliente.estado}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
