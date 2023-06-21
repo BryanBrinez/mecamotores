406756f7dfec63ea2e5aa050d077802ae4646c86
BryanBrinez <94941570+BryanBrinez@users.noreply.github.com>
2023-06-21 16:03:02 -0500

    acualizando precio de los repuestos

--- app/api/repuestos/usado/route.js
+++ app/api/repuestos/usado/route.js
@@ -1,19 +1,45 @@
 import RepuestoUsado from "@models/repuestoUsado";
+import Repuesto from "@models/repuesto";
+import Servicio from "@models/servicio";
 import connectToDB from "@utils/database";
 
 export const POST = async (request) => {
   const { idService, idRep, cantReps,nameRep } = await request.json();
 
+
+
+
+
   try {
     await connectToDB();
 
-    //console.log(idService);
-    //console.log(idRep);
-    //console.log(cantReps);
+    //rep
+    const rep = await Repuesto.findOne({ _id: idRep });
+    const precio =  parseInt(rep.precio) * parseInt(cantReps);
+
+    const filter = { _id: idRep };
+    const update = { valorRepuestos: precio };
+
+    
+    const actualizacionPrecioRep = await Servicio.updateOne(filter,update);
+    //
+    
+   
 
     const result = await RepuestoUsado.findOne({ servicio: idService });
 
     if (result) {
+      const repFound = await Repuesto.findOne({ _id: idRep });
+
+      const precio =  parseInt(repFound.precio) * parseInt(cantReps);
+
+      const filter = { _id: idService };
+      const update = { valorRepuestos: precio };
+  
+      
+      const actualizacionPrecioRep = await Servicio.updateOne(filter,update);
+
+
       const rep = await RepuestoUsado.updateOne(
         { servicio: idService },
         {
